dadaaba615759a4ac717aa7639cade69dfcdac1c
Jurgen <jurgen.geitner@gmail.com>
2017-04-01 16:26:40 +0200

    Basic stack created

--- Stack/Stack.playground/Contents.swift
+++ Stack/Stack.playground/Contents.swift
@@ -6,7 +6,34 @@ var str = "Hello, playground"
 
 var name = "jurgen"
 
-let maxSpeed = 1000
+//Stack is a LIFO structure. Lets make it by putting a wrapper around an array
+public struct Stack<T> {
+    fileprivate var array = [T]()
+    
+    public var isEmpty: Bool {
+        return array.isEmpty ///built in to Swift arrays
+    }
+    
+    public var count: Int {
+        return array.count
+    }
 
+    public mutating func push(_element: T){ //WTF is a mutating func?
+        array.append(_element)
+    }
+    
+    public mutating func pop() -> T? { //optional ?
+        return array.popLast()
+    }
+    
+    public var top: T? { //also optional
+        return array.popLast()
+    }
 
-//test
+}
+
+//let's push a number onto the stack
+//stack.push(9)
+
+//The stack is now [9]. Lets push 5 on
+
