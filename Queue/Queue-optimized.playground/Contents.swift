071bf15c653038e163aef6a8cd2b48ec8159f3ed
Jurgen <jurgen.geitner@gmail.com>
2017-04-05 18:02:45 +0200

    Optimized queue started, now lets test

--- Queue/Queue-optimized.playground/Contents.swift
+++ Queue/Queue-optimized.playground/Contents.swift
@@ -7,3 +7,54 @@ import UIKit
 //e.g. [xxx,xxx, "Jurgen", "R2D2", xxx, xxx]
 //Then we trim empty spots in array like this ["Jurgen", "R2D2", xxx, xxx]
 //Dequeing becomes O(1) (on average)
+
+public struct Queue<T> {
+    fileprivate var array = [T?]()//why fileprivate?
+    fileprivate var head = 0
+    
+    public var isEmpty: Bool{
+        return count == 0
+    }
+    
+    public var count: Int{
+        return array.count - head //that is this count  - head syntax?
+    }
+    
+    
+    public mutating func enqueue(_ element: T){
+        array.append(element)
+    }
+    
+    public mutating func dequeue()-> T?{
+        guard head < array.count, let element = array[head] else { return nil}
+        
+        array[head] = nil
+        head  += 1 //add 1 to the head
+        
+        let percentage = Double(head)/Double(array.count)
+        if array.count > 50 && percentage > 0.25 { //why were these values chosen?
+            array.removeFirst(head)
+            head = 0
+        }
+        
+        return element
+    }
+    
+    public var front: T? {
+        if isEmpty{
+            return nil
+        } else{
+            return array[head]
+        }
+    }
+
+}
+
+
+//Ok lets test it1
+
+//Enqueue a number
+
+queue.enque(10)
+
+
